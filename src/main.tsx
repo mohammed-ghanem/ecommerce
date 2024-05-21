5dadb682b03365f1598bc5d5028087638579f3cb
mohammed-ghanem <mohammed.ghanem240@gmail.com>
2024-05-21 21:29:28 +0300

    newchange

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,8 @@
 import ReactDOM from 'react-dom/client'
-import Header from "./components/layouts/Header.tsx"
+
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
 
-  <Header/>
+  <div> home page</div>
 )
+
