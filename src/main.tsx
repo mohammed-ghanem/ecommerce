a1b721456c323686de9cac8f2228a548f075bf4d
mohammed-ghanem <mohammed.ghanem240@gmail.com>
2024-05-21 21:25:47 +0300

    create header

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import ReactDOM from 'react-dom/client'
+import Header from "./components/layouts/Header.tsx"
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
 
-  <div> home page</div>
+  <Header/>
 )
